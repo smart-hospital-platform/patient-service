7f2a29ac3f6f21f8023987c975952d286fb0e1e0
Kottala <sandeep.kottala@accenture.com>
2026-07-24 16:18:52 +0530

    Created Patient API

---
+++ src/main/java/com/hospitalmanagement/patient_service/controller/PatientController.java
@@ -0,0 +1,56 @@
+package com.hospitalmanagement.patient_service.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hospitalmanagement.patient_service.entity.Patient;
+import com.hospitalmanagement.patient_service.service.PatientService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/patients")
+@Slf4j
+public class PatientController {
+	
+	private final PatientService patientService;
+
+	public PatientController(PatientService patientService) {
+		this.patientService = patientService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
+		
+		log.info("Create Patient API request is received");
+		Patient createdPatient = patientService.createPatient(patient);
+		log.info("Create Patient API completed successfully!");
+		return ResponseEntity.ok(createdPatient);
+	}
+	
+	@GetMapping("/getAllPatients")
+	public List<Patient> getAllPatients() {
+		log.info("Get All Patients API request is received");
+		List<Patient> patients = patientService.getAllPatients();
+		log.info("Get All Patients API completed sucessfully. Total patients fetched : {}", patients.size());
+		return patients;
+	}
+	
+	@GetMapping("/{patientId}")
+	public Optional<Patient> getPatientById(@PathVariable String patientId) {
+		log.info("Request received to get Patient details");
+		Optional<Patient> patient = patientService.getPatientById(patientId);
+		log.info("Successfully feteched patient details");
+		return patient;
+	}
+	
+
+}
---
+++ src/main/java/com/hospitalmanagement/patient_service/service/PatientService.java
@@ -0,0 +1,16 @@
+package com.hospitalmanagement.patient_service.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.hospitalmanagement.patient_service.entity.Patient;
+
+
+public interface PatientService {
+
+	Patient createPatient(Patient patient);
+	List<Patient> getAllPatients();
+	Optional<Patient> getPatientById(String patientId);
+}
