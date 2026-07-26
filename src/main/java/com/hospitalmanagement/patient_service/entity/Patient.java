f20d7d061876a92612aae6eb8f6bc85eb53a7f15
Kottala <sandeep.kottala@accenture.com>
2026-07-26 22:09:58 +0530

    added patientDTO class

--- src/main/java/com/hospitalmanagement/patient_service/entity/Patient.java
+++ src/main/java/com/hospitalmanagement/patient_service/entity/Patient.java
@@ -17,42 +17,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Patient {
 
-    @Id
-    @NotBlank(message = "Patient ID cannot be blank")
-    private String patientId;
+	@Id
+	private String patientId;
 
-    @NotBlank(message = "Patient Name cannot be blank")
-    private String patientName;
+	private String patientName;
 
-    @Min(value = 1, message = "Age must be greater than 0")
-    @Max(value = 120, message = "Age cannot be greater than 120")
-    private Integer age;
+	private Integer age;
 
-    @NotBlank(message = "Gender cannot be blank")
-    @Pattern(
-        regexp = "Male|Female|Other",
-        message = "Gender must be Male, Female or Other"
-    )
-    private String gender;
+	private String gender;
 
-    @NotBlank(message = "Mobile Number cannot be blank")
-    @Pattern(
-        regexp = "^[0-9]{10}$",
-        message = "Mobile Number must contain exactly 10 digits"
-    )
-    private String mobileNumber;
+	private String mobileNumber;
 
-    @NotBlank(message = "Blood Group cannot be blank")
-    @Pattern(
-        regexp = "^(A|B|AB|O)[+-]$",
-        message = "Invalid Blood Group"
-    )
-    private String bloodGroup;
+	private String bloodGroup;
 
-    @NotBlank(message = "Address cannot be blank")
-    private String address;
+	private String address;
 
-    @Email(message = "Invalid Email Format")
-    @NotBlank(message = "Email cannot be blank")
-    private String email;
+	private String email;
 }
